06a27ef5b4833bf39108a0904ddb768e1db53da8
mattbrailsford <me@mattbrailsford.com>
2018-12-27 19:34:36 +0000

    Created smart-link as an easier to user wrapper around NavLink

---
+++ docs/.vuepress/components/SmartLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav-link :item="linkItem" />
+</template>
+
+<script>
+import NavLink from '../theme/NavLink'
+
+export default {
+    props: [ 'text', 'href' ],
+    components: { NavLink },
+    computed: {
+        linkItem() {
+            return {
+                text: this.text,
+                link: this.href
+            }
+        }
+    }
+}
+</script>
--- docs/.vuepress/components/WorkInProgress.vue
+++ docs/.vuepress/components/WorkInProgress.vue
@@ -1,22 +1,13 @@
 <template>
     <div class="tip custom-block">
         <p class="custom-block-title">Work in Progress</p>
-        <p>If you would like to help write the documentation for this section, you can find details on how over at our <nav-link :item="gitHubLink" />.</p>
+        <p>If you would like to help write the documentation for this section, you can find details on how over at our <smart-link text="GitHub repository" :href="repoUrl" />.</p>
     </div>
 </template>
 
 <script>
-import NavLink from '../theme/NavLink'
-
 export default {
-    components: { NavLink },
     computed: {
-        gitHubLink() {
-            return {
-                text: "GitHub repository",
-                link: this.repoUrl
-            }
-        },
         repoUrl() {
             return "https://github.com/" + this.$site.themeConfig.repo
         }
