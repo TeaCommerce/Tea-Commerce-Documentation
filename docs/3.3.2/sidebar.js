f4fa1313c186403eaad06606a51ecb37b9a6b8a4
mattbrailsford <me@mattbrailsford.com>
2019-09-02 11:22:34 +0100

    3.3.2 draft

---
+++ docs/3.3.2/sidebar.js
@@ -0,0 +1,66 @@
+module.exports = [
+    {
+      title: 'Getting Started',
+      children: [
+        'getting-started/installation/',
+        'getting-started/configuration/',
+        'getting-started/user-interface/',
+      ]
+    },
+    {
+      title: 'Guides',
+      children: [
+        'guides/building-an-ecommerce-site/',
+      ]
+    },
+    {
+      title: 'API',
+      children: [
+        'api/',
+        'api/store/',
+        'api/order/',
+        'api/order-line/',
+        'api/order-property/',
+        'api/currency/',
+        'api/country/',
+        'api/country-region/',
+        'api/shipping-method/',
+        'api/payment-method/',
+        'api/payment-forms/',
+        'api/vat-group/',
+        'api/gift-card/',
+        'api/discount-code/',
+        'api/product/',
+        'api/events/',
+        'api/extending-tea-commerce/',
+      ]
+    },
+    {
+      title: 'Payment Providers',
+      children: [
+        'payment-providers/',
+        'payment-providers/2checkout/',
+        'payment-providers/authorizenet/',
+        'payment-providers/axcess/',
+        'payment-providers/cybersource/',
+        'payment-providers/dibs/',
+        'payment-providers/epay/',
+        'payment-providers/invoicing/',
+        'payment-providers/klarna/',
+        'payment-providers/netaxept/',
+        'payment-providers/ogone/',
+        'payment-providers/payer/',
+        'payment-providers/payex/',
+        'payment-providers/paymentsense/',
+        'payment-providers/paynova/',
+        'payment-providers/paypal/',
+        'payment-providers/quickpay-v10/',
+        'payment-providers/quickpay/',
+        'payment-providers/sage-pay/',
+        'payment-providers/stripe/',
+        'payment-providers/stripe-subscription/',
+        'payment-providers/wannafind/',
+        'payment-providers/worldpay/',
+      ]
+    }
+  ]
